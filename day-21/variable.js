a669d3138a73b71d5a93c43089e3ebc8a84b1f94
Akshay-1001 <pichaisundar318@gmail.com>
2023-09-16 09:28:31 +0530

    added day 21 files

---
+++ day-21/variable.js
@@ -0,0 +1,11 @@
+function Start() {
+    aa = 30;
+    var GlobalVar = 48;
+    let LocalVar = 58;
+    const ConstVar = 10;
+
+    console.log(GlobalVar);
+    console.log(LocalVar);
+    console.log(ConstVar);
+    console.log(aa);
+}
